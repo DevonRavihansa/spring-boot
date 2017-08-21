b0ac9bd39e2486d686d2373a407e72ed53d7eb08
Devon Ravihansa <devonravihansawiz@gmail.com>
2017-08-21 15:51:59 +0530

    Create DefaultController.java

---
+++ DefaultController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Devon Ravihansa on 8/5/2017.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("pageTitle", "Spring Thymeleaf Home Page");
+        model.addAttribute("content", "dashboard");
+        return "general";
+    }
+
+    @RequestMapping("/login")
+    public String signIn(Model model){
+        model.addAttribute("pageTitle", "Login Page");
+        model.addAttribute("content", "login");
+        return "general";
+    }
+}
